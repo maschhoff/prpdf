0178d43f659f08bf7834adf7ee6c99f8aa33cc7c
Mathias Aschhoff <4007829+maschhoff@users.noreply.github.com>
2024-12-07 07:32:11 +0100

    Update prpdf.py

--- prpdf.py
+++ prpdf.py
@@ -221,9 +221,9 @@ def loadFiles():
                 for file in files:
                         filer={}
                         filer["name"]=file.rsplit("/",1)[1]
-                        filer["size"]=str(os.path.getsize(file)/1000000)+" MB"
+                	filer["size"]=str(os.path.getsize(file)/1000000)+" MB"
 			timestamp = datetime.fromtimestamp(os.path.getmtime(file)).strftime('%Y-%m-%d %H:%M:%S')
-                        #timestamp = date.fromtimestamp(os.path.getmtime(file))
+			#timestamp = date.fromtimestamp(os.path.getmtime(file))
                         filer["date"]=timestamp
                         res.append(filer)
         
